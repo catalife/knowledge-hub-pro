5f15979f643561e8618eeb5c97d49e14cd553440
catalife <dshantanu434@gmail.com>
2026-03-04 10:45:03 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/knowledge-hub-pro/',
   plugins: [react()],
   test: {
     environment: "jsdom",
