665ec38fdcb54c44a0a3f546c08bdfb563047c8e
catalife <dshantanu434@gmail.com>
2026-03-04 11:20:48 +0530

    Update Admin.tsx: Replace tech courses with clinical research courses

---
+++ Admin.tsx
@@ -0,0 +1,10 @@
+const adminCourses = [
+    'Introduction to Clinical Research',
+    'Advanced Clinical Trial Management',
+    'Biostatistics for Clinical Research',
+    'Ethics in Clinical Research',
+    'Regulatory Affairs in Clinical Trials',
+    'Data Management in Clinical Research',
+];
+
+export default adminCourses;
